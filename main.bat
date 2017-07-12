1e5f391aec145662df9c55eabc55aa39c1458090
WeiZhiHuang <s1410432005@gms.nutc.edu.tw>
2017-07-12 15:00:30 +0800

    tmp

--- main.bat
+++ main.bat
@@ -1,9 +1,14 @@
 @echo off
+del "%~dp1*.csv"
 for %%i in (%*) do (
     @if not "%%~xi"==".pdf" (
-        echo %%~ni%%~xi is not a PDF file...
+        echo %%~nxi is not a PDF file...
     ) else (
         node "%~dp0/main" %%i "%%~ni.csv"
     )
 )
+::copy "%~dp0title.csv"+"%~dp1*.csv" "%~dp1all.csv"
+::attrib +r "%~dp1all.csv"
+::del "%~dp1*.csv"
+::attrib -r "%~dp1all.csv"
 pause
